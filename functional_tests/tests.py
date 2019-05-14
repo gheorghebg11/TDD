351002e8d464d20ae6584f74abbc2cc77f59f39d
gheorghebg11 <gheorghebg@gmail.com>
2019-05-14 14:40:42 +0200

    add 2nd user, with each their list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,38 +39,57 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')        
 
 
-	    # She types "Buy photo album" into a text box
+	    # She types "Make photo album" into a text box
         inputbox.send_keys('Make photo album')
 	    
-        # When she hits enter the page updates and now the page lists "1:Make photo album" as an item in a to-do list
+        # When she hits enter the page updates she is taken to a new URL and now the page lists "1:Make photo album" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(0.5)
+        christa_list_url = self.browser.current_url
+        self.assertRegex(christa_list_url, '/lists/.+')
 
-        self.check_for_row_in_list_table('1: Make photo album')
 
-
-        # She types "Make another photo album" into a text box
+        # There is still a textox inviting her to add another item. She enters "Make another photo album" 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Make another photo album')
 
-	    # When she hits enter the page updates and now the page lists "1:Make another photo album" as an item in a to-do list
+    	# The page updates again and now shows both items on her list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
+        time.sleep(0.5)        
+        self.check_for_row_in_list_table('1: Make photo album')
         self.check_for_row_in_list_table('2: Make another photo album')
 
+	   
+        # Now a new user, Bogdan, comes along to the site
+        ## we use a new browser session to make sure that no info is shared through coookies, etc        
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
 
-	    # There is still a textox inviting her to add another item. She enters "Make another photo album" 
+        # Bogdan visits the homepage, there is no sign of Christa's list
+        self.browser.get(self.live_server_ur)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Make photo album', page_text)
+        
+        # Bogdan starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Bogdan gets his own unique UrL
+        bogdan_list_url = self.browser.current_url
+        self.assertRegex(bogdan_list_url, '/lists/.+')
+        self.assertNotEqual(bogdan_list_url, christa_list_url)
+
+        # Again there is no trace of Christa's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Make photo album', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied they both go back to sleep
+
+
         self.fail('Finish the test!')
 
-    	# The page updates again and now shows both items on her list
-
-
-
-	    # Christa wonders wheter the site will rememer her list. The she sees that the site has generated a unique URL for her
-
-
-    	# She visits that URL - the to-do list is still there!
 
 #if __name__ == '__main__':
 #    unittest.main() #warnings ='ignore')
