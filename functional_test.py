7207f8d19aefeacfd6a5ecf5b4c20578dee9579c
gheorghebg11 <gheorghebg@gmail.com>
2019-05-14 11:33:08 +0200

    enter to-do item works, needs refactoring to add index

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,12 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied she goes back to sleep
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,  [row.text for row in rows], "todo item didn't appear in table, text was: \n%s" %table.text)
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 	    # Christa heard about a cool new page, she goes and check it out!
         self.browser.get('http://localhost:8000')
@@ -37,10 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter the page updates and now the page lists "1:Buy photo album" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy photo album',  [row.text for row in rows], "todo item didn't appear in table, text was: \n%s" %table.text)
+        
+        self.check_for_row_in_list_table('1: Make photo album')
 
 
         # She types "Buy another photo album" into a text box
@@ -50,11 +54,10 @@ class NewVisitorTest(unittest.TestCase):
 	    # When she hits enter the page updates and now the page lists "1:Buy another photo album" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        
+        check_for_row_in_list_table(self, '2: Make another photo album')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Buy another photo album',  [row.text for row in rows], "todo item didn't appear in table, text was: \n%s" %table.text)
-    
+
 	    # There is still a textox inviting her to add another item. She enters "Make another photo album" 
         self.fail('Finish the test!')
 
