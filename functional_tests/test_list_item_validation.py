e31a8302c4c499e2eda354bb6564032d9cb2aab8
gheorghebg11 <gheorghebg@gmail.com>
2019-05-17 11:38:59 +0200

    also reorganized unit tests into a folder

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,15 +13,31 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 class ItemValidationTest(FunctionalTest):    
     def test_cannot_add_empty_list_items(self):
         # Christa goes to the home pae and tries to submit an empty list
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # The home page refreshes and there is a an error message saying that the list cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with text and now it works
+        self.broswer.find_element_by_id('id_new_item').send_keys('Buy photo album\n')
+        self.check_for_row_in_list_table('1: Buy photo album')
 
         # She tries to submit an empty message again
+        self.broswer.find_element_by_id('id_new_item').send_keys('\n')
 
         # Same warning comes
+        self.check_for_row_in_list_table('1: Buy photo album')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
 
         # She can correct it by filling some text in
+        self.broswer.find_element_by_id('id_new_item').send_keys('Buy another photo album\n')
+        self.check_for_row_in_list_table('1: Buy photo album')
+        self.check_for_row_in_list_table('2: Buy another photo album')
+
+
         self.fail('write me!')
 
--- lists/tests/test_all.py
+++ lists/tests/test_all.py
@@ -76,7 +76,7 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' %(correct_list.id))
-        self.assertEqual(response.context['list'], correct_list)
+
         
 
 class NewListTest(TestCase):
