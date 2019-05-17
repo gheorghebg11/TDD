2b0506af1e30b3c7690a61648ebe64ef5eec19df
gheorghebg11 <gheorghebg@gmail.com>
2019-05-17 11:42:43 +0200

    reorgnized unit tests in different files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -22,32 +22,7 @@ class HomePageTest(TestCase):
         #self.assertEqual(response.content, expected_response.content)
         #expected_html = render_to_string('home.html', request=request)# {'new_item_text':'A new list item'}, request=request)
         #self.assertEqual(response.content.decode(), expected_html)
-        
 
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        self.assertEqual(saved_items[0].text, 'The first (ever) list item')
-        self.assertEqual(saved_items[1].text, 'Item the second')
-        self.assertEqual(saved_items[0].list, list_)
-        self.assertEqual(saved_items[1].list, list_)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
